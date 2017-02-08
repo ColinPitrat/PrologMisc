d780f3f69a9b79460c0c4f7e4848da516ee835e7
Colin Pitrat <colin.pitrat@gmail.com>
2017-02-08 20:17:19 +0000

    Add a simple (and wrong) in_line predicate.

--- Geometry/Geometry.pl
+++ Geometry/Geometry.pl
@@ -13,3 +13,15 @@ horizontal_line(P1, P2):-
 	line(P1, P2),
 	P1 = point(_, Y),
 	P2 = point(_, Y).
+
+circle(P, R):-
+  P = point(_, _),
+  R > 0.
+
+in_line(P, L):-
+  L = line(point(X1, Y1), point(X2, Y2)),
+  P = point(X, Y),
+  /* TODO: what if X2 == 0 or Y2 == 0 ?
+           what if X2 == 0 and Y1 == 0 ? */
+  K = (X - X1)/X2,
+  K =:= (Y - Y1)/Y2.
