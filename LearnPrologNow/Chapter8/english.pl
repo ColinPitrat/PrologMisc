4015d0ebb733729477df19f754572b0a7a29b918
Colin Pitrat <colin.pitrat@gmail.com>
2017-02-17 21:14:26 +0000

    Add adjectives and prepositional.

--- LearnPrologNow/Chapter8/english.pl
+++ LearnPrologNow/Chapter8/english.pl
@@ -1,15 +1,23 @@
-s(_, N, G, P)  -->  np(subject, N, G, P),vp(_, N, G, P). 
+s(_, N, G, P)  -->  np(subject, N, G, P),prepositional(_, _, _, _),vp(_, N, G, P). 
 
-np(S, N, G, P)  -->  det(S, N, G, P),noun(S, N, G, P). 
+np(S, N, G, P)  -->  det(S, N, G, P),adjectives(S, N, G, P, []),noun(S, N, G, P).
 np(S, N, G, P)  -->  pronoun(S, N, G, P).
 
-vp(_, N, G, P)  -->  verb(_, N, G, P),np(object, _, _, _). 
+adjectives(_, _, _, _, _) --> [].
+adjectives(S, N, G, P, L) --> { adjective(S, N, G, P, [A], []), \+member(A, L)}, [A], adjectives(S, N, G, P, [A | L]). 
+
+prepositional(_, _, _, _) --> [].
+prepositional(S, N, G, P) --> preposition(S, N, G, P), np(object, N, G, P).
+
+vp(_, N, G, P)  -->  verb(_, N, G, P),np(object, _, _, _),prepositional(_, _, _, _). 
 vp(_, N, G, P)  -->  verb(_, N, G, P). 
 
 det(S, N, G, P) --> [Word], {lex(Word, det(S, N, G, P))}.
 noun(S, N, G, P) --> [Word], {lex(Word, noun(S, N, G, P))}.
 verb(_, N, G, P) --> [Word], {lex(Word, verb(_, N, G, P))}.
 pronoun(S, N, G, P) --> [Word], {lex(Word, pronoun(S, N, G, P))}.
+adjective(S, N, G, P) --> [Word], {lex(Word, adjective(S, N, G, P))}.
+preposition(S, N, G, P) --> [Word], {lex(Word, preposition(S, N, G, P))}.
 
 /* Lexicon */
 lex('I',  pronoun(subject, 1, _,         singular)).
@@ -40,6 +48,9 @@ lex(but, conj).
 lex(woman, noun(_, 3, _, singular)).
 lex(man, noun(_, 3, _, singular)).
 lex(dog, noun(_, 3, _, singular)).
+lex(cow, noun(_, 3, _, singular)).
+lex(shower, noun(_, 3, _, singular)).
+lex(table, noun(_, 3, _, singular)).
 lex(women, noun(_, 6, _, plural)).
 lex(men, noun(_, 6, _, plural)).
 lex(dogs, noun(_, 6, _, plural)).
@@ -48,3 +59,12 @@ lex(shoots, verb(_, 3, _, singular)).
 lex(shoot,  verb(_, 1, _, _)).
 lex(shoot,  verb(_, 2, _, _)).
 lex(shoot,  verb(_, _, _, plural)).
+
+lex(big, adjective(_, _, _, _)).
+lex(fat, adjective(_, _, _, _)).
+lex(small, adjective(_, _, _, _)).
+lex(frightened, adjective(_, _, _, _)).
+
+lex(on, preposition(_, _, _, _)).
+lex(under, preposition(_, _, _, _)).
+lex('next to', preposition(_, _, _, _)).
